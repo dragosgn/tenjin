435af88e541cae656c5e2cb622b644de89af5079
Dragos <dragosgn@gmail.com>
2017-09-29 10:58:53 +0200

    Working enviroment

--- client/index.js
+++ client/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom'
 import App from './components/App.js'
 
 
-ReactDOM.render(<App />, document.getelementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
