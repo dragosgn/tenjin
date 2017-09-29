88f86cbe8a7f8df7a4fde9b62aa88790e76c07f2
Dragos <dragosgn@gmail.com>
2017-09-29 10:45:39 +0200

    finished webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path')
 
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
 mdoule.exports = {
   entry: './client/index.js',
   output: {
@@ -11,5 +18,6 @@ mdoule.exports = {
       { test:  /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test:  /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 }
