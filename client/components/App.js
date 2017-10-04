08507c1f28dd552dfc45070f4fd98cf474735343
Dragos <dragosgn@gmail.com>
2017-10-04 10:28:31 +0200

    initial text box

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react'
+import TextBox from './TextBox'
+
 
 export default class App extends Component {
   render() {
     return(
       <div>
         <h1>Tenjin</h1>
+        <TextBox />
       </div>
     )
   }
--- client/components/TextBox.js
+++ client/components/TextBox.js
@@ -1,1 +1,21 @@
 import React from 'react'
+import compose from 'recompose'
+import styled from 'styled-componets'
+
+const Root = styled.div`
+
+`
+
+const TextBox = () => (
+  <Root>
+    <div>
+      <h1>Write here</h1>
+      <input type="text"></input>
+    </div>
+  </Root>
+)
+
+
+export default compose(
+
+)(TextBox)
