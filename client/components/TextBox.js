e2f914748e8691ba7953b5c2d8defd5edaf97b6e
Dragos <dragosgn@gmail.com>
2017-10-04 10:46:32 +0200

    text box

--- client/components/TextBox.js
+++ client/components/TextBox.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import compose from 'recompose'
-import styled from 'styled-componets'
+import {compose} from 'recompose'
+import styled from 'styled-components'
 
 const Root = styled.div`
 
