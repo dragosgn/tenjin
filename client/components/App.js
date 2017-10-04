a502120c3449978f2c6e7eaf11083c77fe4d3a7f
Dragos <dragosgn@gmail.com>
2017-10-04 10:51:12 +0200

    changed textbox

--- client/components/App.js
+++ client/components/App.js
@@ -6,7 +6,6 @@ export default class App extends Component {
   render() {
     return(
       <div>
-        <h1>Tenjin</h1>
         <TextBox />
       </div>
     )
--- client/components/TextBox.js
+++ client/components/TextBox.js
@@ -3,14 +3,21 @@ import {compose} from 'recompose'
 import styled from 'styled-components'
 
 const Root = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
+const StyledInput = styled.input`
+  border-radius: 5px;
+  border: 1px solid grey;
 `
 
 const TextBox = () => (
   <Root>
     <div>
       <h1>Write here</h1>
-      <input type="text"></input>
+      <StyledInput type="text"></StyledInput>
     </div>
   </Root>
 )
