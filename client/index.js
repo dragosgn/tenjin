00f0a3f2827bd7f4f7b41756919b236756ab2854
Dragos <dragosgn@gmail.com>
2017-10-05 10:12:53 +0200

    added redux forms

--- client/index.js
+++ client/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App.js'
-import { createStore } from 'redux'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
 import { Provider } from 'react-redux'
-import reducer from './reducers'
+import { reducer as formReducer } from 'redux-form'
 import {ThemeProvider} from 'styled-components'
+
+import reducer from './reducers'
+
+
+const rootReducer = combineReducers({
+  form: formReducer.plugin({
+    writer: writer
+  })
+})
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <Provider store={store}>
