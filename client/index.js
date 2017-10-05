d50db392a6114e5030f1e8c8d6b97518fda8b144
Dragos <dragosgn@gmail.com>
2017-10-05 10:07:07 +0200

    added redux provider and theme provider

--- client/index.js
+++ client/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App.js'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import {ThemeProvider} from 'styled-components'
 
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'))
