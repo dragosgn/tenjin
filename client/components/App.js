61b48af1dd5c3559dd27b35193c06b9d2cd20cce
Dragos <dragosgn@gmail.com>
2017-11-03 14:22:17 +0100

    styling

--- client/components/App.js
+++ client/components/App.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react'
 import TextBox from './TextBox'
 import {compose} from "recompose"
+import styled from "styled-components"
 
+const Root = styled.div`
+  padding: 1rem;
+`
 
 export default class App extends Component {
   render() {
     return(
-      <div>
+      <Root>
+        <h3>The most advanced writer on the web: Tenjin</h3>
         <TextBox />
-      </div>
+      </Root>
     )
   }
 }
